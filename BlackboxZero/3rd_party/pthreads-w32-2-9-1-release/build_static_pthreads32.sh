dfe1e1bb80764ddb98cbb599129b28577fc5f6ed
Mojmir Svoboda <mojmir_svoboda@users.berlios.de>
2013-12-01 16:31:02 +0100

    * cmake mingw debug

--- BlackboxZero/3rd_party/pthreads-w32-2-9-1-release/build_static_pthreads32.sh
+++ BlackboxZero/3rd_party/pthreads-w32-2-9-1-release/build_static_pthreads32.sh
@@ -2,5 +2,5 @@
 cd pthreads.2/
 make clean GC-static
 mingw_prefix=/c/MinGW
-cp libpthreadGC2.a $mingw_prefix/lib/libpthread.a
-cp pthread.h sched.h semaphore.h $mingw_prefix/include
+cp -v libpthreadGC2.a $mingw_prefix/lib/libpthread.a
+cp -v pthread.h sched.h semaphore.h $mingw_prefix/include
