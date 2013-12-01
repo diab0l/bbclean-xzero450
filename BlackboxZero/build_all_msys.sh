9fec981ae5b6388270f874e0a9f8b991b4ca7363
Mojmir Svoboda <mojmir_svoboda@users.berlios.de>
2013-12-01 11:32:43 +0100

    * cmake script debug

--- BlackboxZero/build_all_msys.sh
+++ BlackboxZero/build_all_msys.sh
@@ -6,7 +6,9 @@ mkdir _projects.msys.32
 cd _projects.msys.32
 export PATH=/c/mingw/bin:$PATH
 cmake -G "MSYS Makefiles" -DCMAKE_INSTALL_PREFIX:PATH=c:/bbZero.msys32 ..
+[[ $? -ne 0 ]] && exit 1
 make all install
+[[ $? -ne 0 ]] && exit 1
 
 cd ..
 
@@ -15,6 +17,8 @@ mkdir _projects.msys.64
 cd _projects.msys.64
 export PATH=/c/mingw64/bin:$PATH
 cmake -G "MSYS Makefiles" -DCMAKE_INSTALL_PREFIX:PATH=c:/bbZero.msys64 ..
+[[ $? -ne 0 ]] && exit 1
 make all install
+[[ $? -ne 0 ]] && exit 1
 
 
--- BlackboxZero/run_generator_msys.sh
+++ BlackboxZero/run_generator_msys.sh
@@ -6,6 +6,7 @@ mkdir _projects.msys.32
 cd _projects.msys.32
 export PATH=/c/mingw/bin:$PATH
 cmake -G "MSYS Makefiles" -DCMAKE_INSTALL_PREFIX:PATH=c:/bbZero.msys32 ..
+[[ $? -ne 0 ]] && exit 1
 
 cd ..
 
@@ -14,5 +15,6 @@ mkdir _projects.msys.64
 cd _projects.msys.64
 export PATH=/c/mingw64/bin:$PATH
 cmake -G "MSYS Makefiles" -DCMAKE_INSTALL_PREFIX:PATH=c:/bbZero.msys64 ..
+[[ $? -ne 0 ]] && exit 1
 
 
